130fb0f8055ef08db38e161588affefaf3570160
nazmul-rdo <mdnazmulkhanrdo@gmail.com>
2024-03-01 06:56:36 +0600

    complate the design

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
 ScrollTrigger.create({
   start: 'top -100',
   end: 99999,
-  toggleClass: {className: 'header--scrolled', targets: '.header'}
+  toggleClass: { className: 'header--scrolled', targets: '.header' }
+});
+
+const cards = document.querySelectorAll('.cards');
+
+
+cards.forEach(card=> {
+  gsap.to(card, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: card,
+      start: 'top bottom',
+      end: 'bottom top',
+      onEnter: () => card.style.opacity = 1,
+      onLeave: () => card.style.opacity = 0
+    }
+  });
 });
